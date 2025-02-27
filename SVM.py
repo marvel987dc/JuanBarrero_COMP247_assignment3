db42fa45910cc27f939e5e6c0518dd6d7e7b999f
ReactRepo <barrerojuan810@gmail.com>
2025-02-27 15:36:24 -0500

    plotting and splitting the data to train it

--- SVM.py
+++ SVM.py
@@ -41,8 +41,27 @@ plt.show()
 
 #plottin the data pandas
 plt.figure(figsize=(10, 5))
+#bar plot to vizualize teh distribution of values int he 'Class' column
 data_juanDavid['class'].value_counts().plot(kind='bar', color='green')
 plt.title("Distribution of Classes")
 plt.xlabel("Class (2=Benign, 4=Malignant)")
 plt.ylabel("Count")
 plt.show()
+#Separate the features from the class.
+X = data_juanDavid.drop('class', axis=1)
+y = data_juanDavid['class']
+#splitting the data 80% training and 20% testing
+from sklearn.model_selection import train_test_split
+#The seed are the two last digits of my student ID
+seed = 53
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+
+#Training the model
+from sklearn.svm import SVC
+from sklearn.metrics.pairwise import linear_kernel
+#Create the model
+clf_Juan  = SVC(kernel='linear', C = 0.1)
+clf_Juan.fit(X_train, y_train)
+
+
+
